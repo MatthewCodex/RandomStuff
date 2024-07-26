49bf37cda727e91a9b2d9eb9edc1b4d3244cad89
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2024-07-26 16:11:31 -0400

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,1 +1,30 @@
+// Use the following to include this JavaScript file 
+// <script src="script1.js"></script>
+var q = document.createElement('link')
+q.rel = "stylesheet"
+q.href = "https://cdn.jsdelivr.net/gh/MatthewCodex/RandomStuff@4bfb447b3a8272b68a31f6cec28b028eca55b26b/Provider.css"
 
+var w = document.createElement('div')
+w.classList.add("popup")
+
+var e = document.createElement('h3')
+e.innerHTML = "Testing"
+
+var r = document.createElement('p')
+r.innerHTML = "Testing"
+
+var t = document.createElement('a')
+t.classList.add("bh")
+t.innerHTML = "Testing"
+
+var y = document.createElement('a')
+y.classList.add("bh")
+y.innerHTML = "Testing"
+
+document.head.appendChild(q)
+w.appendChild(e)
+w.appendChild(r)
+w.appendChild(t)
+w.appendChild(y)
+document.body.appendChild(w)
+      
