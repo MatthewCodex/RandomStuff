1b36c16971168dc1cfdcd8237072a437c34f3809
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2024-07-30 13:07:51 -0400

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,3 +1,22 @@
 var q = document.createElement('link')
 q.href = "Provider.css"
 document.head.appendChild(q)
+var w = document.createElement('div')
+w.classList.add("popup")
+document.body.appendChild(w)
+var e = document.createElement('h3')
+e.classList.add("popup")
+e.innerHTML = "Hello"
+w.appendChild(e)
+var r = document.createElement('p')
+r.classList.add("popup")
+r.innerHTML = "Just a notification..."
+w.appendChild(r)
+var t = document.createElement('a')
+t.classList.add("popup")
+t.innerHTML = "OK"
+w.appendChild(t)
+var y = document.createElement('a')
+y.classList.add("popup")
+y.innerHTML = "Cancel"
+w.appendChild(y)
